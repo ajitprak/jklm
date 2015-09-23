63b2ca4799cab99d969d6e6d1abb7eff15962ac7
Ajith Prakash <ajiprak@gmail.com>
2015-09-23 07:22:43 +0530

    Login Logic In-Progress

---
+++ Routes/authRouteRegistory.js
@@ -0,0 +1,5 @@
+module.exports = function(app){
+    //app.post.apply(app,{['/login',loginMiddleware]});
+};
+
+
--- app.js
+++ app.js
@@ -3,8 +3,33 @@ var app = express();
 var config = require('./server/config');
 var restAuthorization = require('./server/Auth/restAuthorization');
 //var login = require('./server/Auth/login.js');
+var passport = require('passport');
+var passportLocal = require('passport-local');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var expressSession = require('express-session');
 
+//initializing dependencies for session
+app.use(bodyParser.urlEncoded({extended:false}));
 app.use(restAuthorization);
+app.use(cookieParser());
+app.use(expressSession({
+    secret : process.env.SESSION_SECRET || 'BharathMataKiJAI',
+    resave : false,
+    saveUninitialized : false
+}));
+
+//Initializing passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+//Tell passport how to verify out user
+passport.use(User.authenticate); //TODO User.authenticate
+
+//Tell passport how to serialize and deserialize user
+passport.serialize(User.serialize); //TODO User.serialize
+passport.deserialize(User.deserialize);
+
 app.get('/',function(req,res){
     res.writeHead(200,{'Content-Type' : 'text/html'});
     res.end('Welcome to Hawk Security Services');
