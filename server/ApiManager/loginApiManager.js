d8f2d36cb7be360472a554b2cdddc1182a0fae57
Ajith Prakash <ajiprak@gmail.com>
2015-09-30 23:47:49 +0530

    login bug fix clone-deep

--- server/ApiManager/loginApiManager.js
+++ server/ApiManager/loginApiManager.js
@@ -1,12 +1,14 @@
 var util = require('util');
 var defaultApiManager = require('./defaultApiManager.js');
 var queryUtil = require('../customUtils/queryUtil.js');
+var _ = require('lodash');
 
 var loginApiManager = {};
 //util.inherits(loginApiManager,defaultApiManager);
 loginApiManager.handler = function(queryDefinition,parameters,request,response){
-    queryUtil.addParamsToQuery(queryDefinition,parameters);
-    defaultApiManager.handler(queryDefinition,response); 
+    var query = _.cloneDeep(queryDefinition)
+    queryUtil.addParamsToQuery(query,parameters);
+    defaultApiManager.handler(query,response); 
 };
 
 module.exports = loginApiManager;
