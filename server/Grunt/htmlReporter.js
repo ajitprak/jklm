98816916972ad3ef9ea6e85ad36b300184377ee9
Ajith Prakash <ajiprak@gmail.com>
2016-03-29 00:38:51 +0530

    Refactored jsHint Html reporter code - Added style to table

--- server/Grunt/htmlReporter.js
+++ server/Grunt/htmlReporter.js
@@ -2,18 +2,34 @@ module.exports = {
   reporter: function (res) {
     var len = res.length;
     var str = "";
+    var css = "<style>"+
+              "table {"+
+                "border-collapse: collapse;"+
+                "width: 100%;}"+
+              "th, td {"+
+                "text-align: left;"+
+                "padding: 8px;}"+
+              "th {background-color: #52D017;color: white;}" +
+              "tr:nth-child(even) {background-color: #F0FFFF}"+
+              "tr:hover {background-color: #f5f5f5}" +
+              "</style>";
+    var tableHeader = "<th>File Name</th>"+
+                      "<th>Line Number</th>"+
+                      "<th>Column</th>"+
+                      "<th>Reason</th>"+
+                      "<th>Evidence</th>";
 
     res.forEach(function (r) {
       var file = r.file;
       var err = r.error;
 
-      str += "<tr style='border:1px solid black;'><td style='border:1px solid black;'>" + file + 
-              "</td><td style='border:1px solid black;'> line " + err.line + "</td><td style='border:1px solid black;'> col " 
-              + err.character + "</td><td style='border:1px solid black;'> " + err.reason + "</td></tr>";
+      str += "<tr><td>" + file +
+              "</td><td> line " + err.line + "</td><td> col "
+              + err.character + "</td><td> " + err.reason + "</td><td>"+ err.evidence +"</td></tr>";
     });
 
     if (str) {
-      process.stdout.write("<table style='border:1px solid black;border-collapse:collapse'>" + str + "</table>\n" + len + " error" +
+      process.stdout.write(css + "<table>" + tableHeader + str + "</table>\n" + len + " error" +
         ((len === 1) ? "" : "s") + "\n");
     }
   }
