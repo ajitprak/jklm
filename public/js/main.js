f1f6ab2a080103bafe8cf31071513683a5b7ff0a
Ajith Prakash <ajiprak@gmail.com>
2016-04-05 00:16:01 +0530

    Client Logic Init - In-progress

---
+++ public/js/main.js
@@ -0,0 +1,11 @@
+/**
+ * Created by warlock on 04/04/16.
+ */
+
+require.config({
+    paths:{
+        angular:'lib/angular.min',
+        uiRouter:'lib/angular-ui-router.min',
+        //bootstrap:'lib/bootstrap.css'
+    }
+});
