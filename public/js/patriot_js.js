a7e7b0077b4ea8cd931f70914604edd72df0eabc
Ajith Prakash <ajiprak@gmail.com>
2017-03-30 00:24:29 +0530

    Integrating Login Page UI partial

---
+++ public/js/patriot_js.js
@@ -0,0 +1,73 @@
+function myFunction() {
+    document.getElementById("cg-link-1").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("cg-drop");
+    for (var d = 0; d < dropdowns.length; d++) {
+      var openDropdown = dropdowns[d];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+
+
+$(document).ready(function(){
+
+    var log = function(a){
+        $(".info").html( $(".info").html() + "<br/>" + a );
+    };
+    
+    var toggleClick = function(){
+
+        var divObj = $(this).next();
+        var nstyle = divObj.css("display");
+
+        if(nstyle == "none"){
+            divObj.slideDown(false,function(){
+                $("html").bind("click",function(){
+                    divObj.slideUp();
+                    $("html").unbind("click");
+                });
+            });
+        }
+    };
+
+    $(".clickme").click(toggleClick);
+   
+});
+
+
+
+
+
+jQuery(document).ready(function (e) {
+    function t(t) {
+        e(t).bind("click", function (t) {
+            t.preventDefault();
+            e(this).parent().fadeOut()
+        })
+    }
+    e(".dropdown-toggle").click(function () {
+        var t = e(this).parents(".button-dropdown").children(".dropdown-menu").is(":hidden");
+        e(".button-dropdown .dropdown-menu").hide();
+        e(".button-dropdown .dropdown-toggle").removeClass("active");
+        if (t) {
+            e(this).parents(".button-dropdown").children(".dropdown-menu").toggle().parents(".button-dropdown").children(".dropdown-toggle").addClass("active")
+        }
+    });
+    e(document).bind("click", function (t) {
+        var n = e(t.target);
+        if (!n.parents().hasClass("button-dropdown")) e(".button-dropdown .dropdown-menu").hide();
+    });
+    e(document).bind("click", function (t) {
+        var n = e(t.target);
+        if (!n.parents().hasClass("button-dropdown")) e(".button-dropdown .dropdown-toggle").removeClass("active");
+    })
+});
